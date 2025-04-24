5c063bd7fd607321dc46d0b07691784996182b09
Ravindu Perera <ravindurandika2004@gmail.com>
2025-04-24 22:58:36 +0530

    finshed Auth loggedout

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -104,3 +104,18 @@ export const isAuth = async(req,res)=>{
     }
 }
 
+//logout /api/user/logout
+
+export const logout = async(req,res)=>{
+    try {
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        });
+        return res.json({success: true, message: "Logged Out"})
+    } catch (error) {
+         console.error('Login Error:', error.message);
+        res.status(500).json({ success: false, message: error.message });
+    }
+}
--- server/server.js
+++ server/server.js
@@ -20,7 +20,8 @@ const allowedOrigins=['http://localhost:5173']
 //middleware Configuration
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: allowedOrigins, Credential: true}));
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
 
 
 app.get('/', (req,res)=> res.send("Api Is working"));
